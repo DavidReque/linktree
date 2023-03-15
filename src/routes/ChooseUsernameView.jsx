bc513db82a5cbd9cf0801f610e31cb60d72fd3e2
David Requeno <davidrequeno52@gmail.com>
2023-03-14 18:08:38 -0600

    Estilos corregidos

--- src/routes/ChooseUsernameView.jsx
+++ src/routes/ChooseUsernameView.jsx
@@ -60,7 +60,7 @@ export default function ChooseUsernameView() {
 
   if (currentState === 6) {
     return <div>
-      <h1>Felicidades, ya puedes crear tus links</h1>
+      <h1 className="text-3xl font-bold underline">Felicidades, ya puedes crear tus links</h1>
       <Link to='/dashboard'>Continuar</Link>
     </div>
   }
