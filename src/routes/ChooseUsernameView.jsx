4c0181798ef9165f5a551f8b62a7b76c6d21a550
David Requeno <davidrequeno52@gmail.com>
2023-03-15 17:05:58 -0600

    Boton continuar

--- src/routes/ChooseUsernameView.jsx
+++ src/routes/ChooseUsernameView.jsx
@@ -2,7 +2,7 @@ import { Link, useNavigate } from "react-router-dom";
 import { useState } from "react";
 import AuthProvider from "../components/AuthProvider";
 import { existsUsername, updateUser } from "../firebase/firebase";
-import style from './chooseUsername.module.css'
+import style from "./chooseUsername.module.css";
 
 export default function ChooseUsernameView() {
   const navigate = useNavigate();
@@ -45,25 +45,47 @@ export default function ChooseUsernameView() {
   if (currentState === 3 || currentState === 5) {
     return (
       <div className={style.chooseUsername}>
-        <h1 className="font-normal text-4xl text-white">Bienvenido {currentUser.displayName}</h1>
-        <p className="text-2xl text-slate-400">Para terminar el proceso elige un nombre de usuario</p>
+        <h1 className="font-normal text-4xl text-white">
+          Bienvenido {currentUser.displayName}
+        </h1>
+        <p className="text-2xl text-slate-400">
+          Para terminar el proceso elige un nombre de usuario
+        </p>
         {currentState === 5 ? <p>El nombre de usuario ya existe</p> : ""}
         <div>
-          <input className="m-4 p-3 focus:ring-2 focus:ring-cyan-300 focus:outline-none appearance-none w-full sm:w-auto text-sm text-slate-900 placeholder-slate-800 rounded-md border-2 border-cyan-300 shadow-sm" type="text" onChange={handleInputUsername} />
+          <input
+            className="m-4 p-3 focus:ring-2 focus:ring-cyan-300 focus:outline-none appearance-none w-full sm:w-auto text-sm text-slate-900 placeholder-slate-800 rounded-md border-2 border-cyan-300 shadow-sm"
+            type="text"
+            onChange={handleInputUsername}
+          />
         </div>
 
         <div>
-          <button className="px-5 py-3 rounded-lg border-2 border-cyan-300 bg-cyan-300 font-medium hover:bg-transparent hover:text-white hover:border-2 hover:border-cyan-300" onClick={handleContinue}>Continuar</button>
+          <button
+            className="px-5 py-3 rounded-lg border-2 border-cyan-300 bg-cyan-300 font-medium hover:bg-transparent hover:text-white hover:border-2 hover:border-cyan-300"
+            onClick={handleContinue}
+          >
+            Continuar
+          </button>
         </div>
       </div>
     );
   }
 
   if (currentState === 6) {
-    return <div className="h-screen flex justify-center flex-col items-center">
-      <h1>Felicidades, ya puedes crear tus links</h1>
-      <Link className="my-3 py-2 h-10 px-6 font-semibold rounded-md bg-black text-white hover:bg-slate-600" to='/dashboard'>Continuar</Link>
-    </div>
+    return (
+      <div className={style.validar}>
+        <h1 className="font-normal text-4xl text-white">
+          Felicidades, ya puedes crear tus links
+        </h1>
+        <Link
+          className="px-5 py-3 rounded-lg border-2 border-cyan-300 bg-cyan-300 font-medium hover:bg-transparent hover:text-white hover:border-2 hover:border-cyan-300"
+          to="/dashboard"
+        >
+          Continuar
+        </Link>
+      </div>
+    );
   }
 
   return (
