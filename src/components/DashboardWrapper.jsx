743d0e6eb0f65bc19d2ea7a9368f4eb9db038d5b
David Requeno <davidrequeno52@gmail.com>
2023-03-27 18:56:14 -0600

    Hero estilos

--- src/components/DashboardWrapper.jsx
+++ src/components/DashboardWrapper.jsx
@@ -1,6 +1,8 @@
 import { Link } from "react-router-dom";
 import { useState } from "react";
+
 export default function DashboardWrapper({ children }) {
+  
   const [isOpen, setIsOpen] = useState(false);
 
   return (
